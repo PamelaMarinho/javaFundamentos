b8a5651e8d99dd7c7d75dc7ce0c3c47841f9e11d
pamelamarinho <pamelamarinhopm@hotmail.com>
2022-06-26 18:37:27 -0300

    Annotation

---
+++ src/javafundamentos/aula62annotation/Annotation.java
@@ -0,0 +1,6 @@
+package javafundamentos.aula62annotation;
+
+public @interface Annotation {
+    public String nome = "Souza";
+    public Integer idade = 85;
+}
---
+++ src/javafundamentos/aula62annotation/TesteAnnotation.java
@@ -0,0 +1,9 @@
+package javafundamentos.aula62annotation;
+
+@Annotation
+
+public class TesteAnnotation {
+    public static void main(String[] args) {
+
+    }
+}
